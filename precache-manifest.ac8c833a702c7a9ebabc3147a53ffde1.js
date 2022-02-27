2a38654463176682a7c9b7457bc1a8e2d6ed72d9
cindyweng18 <cindyweng18@gmail.com>
2022-02-26 21:39:02 -0500

    Updates

---
+++ precache-manifest.ac8c833a702c7a9ebabc3147a53ffde1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "86cfbff5b57bc632afc4",
+    "url": "/portfolio/static/css/main.0a2f022a.chunk.css"
+  },
+  {
+    "revision": "86cfbff5b57bc632afc4",
+    "url": "/portfolio/static/js/main.e588959f.chunk.js"
+  },
+  {
+    "revision": "14e95004fb64617a5665",
+    "url": "/portfolio/static/js/runtime~main.15cd5bb7.js"
+  },
+  {
+    "revision": "d35ef815370233316ab5",
+    "url": "/portfolio/static/css/2.a726eb0b.chunk.css"
+  },
+  {
+    "revision": "d35ef815370233316ab5",
+    "url": "/portfolio/static/js/2.301fde9b.chunk.js"
+  },
+  {
+    "revision": "77a17ceacf3be5f46709",
+    "url": "/portfolio/static/js/3.061514ed.chunk.js"
+  },
+  {
+    "revision": "1ed478a6b265d4b4f5c26bb063203588",
+    "url": "/portfolio/static/media/bootstrap-icons.1ed478a6.woff2"
+  },
+  {
+    "revision": "e4cc83ddcec34db65724eb56c5230b4c",
+    "url": "/portfolio/static/media/bootstrap-icons.e4cc83dd.woff"
+  },
+  {
+    "revision": "491365fcfb785ac2049ad8ff92ffd4a0",
+    "url": "/portfolio/index.html"
+  }
+];
