a21460a14125744f496bed190fdb0b4183f7cbc8
Cindy <cindyweng18@gmail.com>
2024-07-22 22:12:05 -0400

    updated gmail and liunkedin handle

--- src/components/about.js
+++ src/components/about.js
@@ -11,11 +11,11 @@ export default class About extends Component {
                 </h1>
                 <div className="subheading mb-5">
                     Albany, NY, 12203 · (646) 479-4588 ·
-                    <a href="mailto:cindybrowning18@gmail.com"> cindybrowning18@aol.com</a>
+                    <a href="mailto:cindybrowning18@gmail.com"> cindybrowning18@gmail.com</a>
                 </div>
                 {/* <p className="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p> */}
                 <div className="social-icons">
-                    <a className="social-icon" href="https://www.linkedin.com/in/cindyweng18/"> <i className="bi bi-linkedin"></i> </a>
+                    <a className="social-icon" href="https://www.linkedin.com/in/cindybrowning18/"> <i className="bi bi-linkedin"></i> </a>
                     <a className="social-icon" href="https://github.com/cindyweng18"> <i className="bi bi-github"></i> </a>
                 </div>
             </div>
