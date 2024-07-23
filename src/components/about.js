f8b7e7a42da6ae0ce41a3207ed728c60a7e2fdad
cindyweng18 <cindyweng18@gmail.com>
2024-07-23 09:39:21 -0400

    added resume viewing link in about page

--- src/components/about.js
+++ src/components/about.js
@@ -13,10 +13,11 @@ export default class About extends Component {
                     Albany, NY, 12203 · (646) 479-4588 ·
                     <a href="mailto:cindybrowning18@gmail.com"> cindybrowning18@gmail.com</a>
                 </div>
-                {/* <p className="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p> */}
+                <p className="lead mb-5">I’m currently a manager at a dental office, where I’ve worked for five years, assisting with patient care, appointment scheduling, and insurance policies. While this role isn’t aligned with my long-term goals, it has improved my communication, problem-solving, and interpersonal skills. I hold a Bachelor’s degree in Computer Science and previously, as a software engineer intern, I gained experience with Python and databases, optimizing ETL pipelines. I aim to transition into a software engineer or data scientist role, particularly in healthcare-focused companies.</p>
                 <div className="social-icons">
-                    <a className="social-icon" href="https://www.linkedin.com/in/cindybrowning18/"> <i className="bi bi-linkedin"></i> </a>
-                    <a className="social-icon" href="https://github.com/cindyweng18"> <i className="bi bi-github"></i> </a>
+                    <a className="social-icon" href="https://www.linkedin.com/in/cindybrowning18/" target="_blank" rel="noopener noreferrer"> <i className="bi bi-linkedin"></i> </a>
+                    <a className="social-icon" href="https://github.com/cindyweng18" target="_blank" rel="noopener noreferrer"> <i className="bi bi-github"></i> </a>
+                    <a className="social-icon" href="https://drive.google.com/file/d/1E1gRS5qeUhyXOWWhNzyBB5UHgd9GAxdr/view?usp=sharing" target="_blank" rel="noopener noreferrer"> <i class="bi bi-file-earmark-text-fill"></i> </a>
                 </div>
             </div>
         </section>
