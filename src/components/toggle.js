db5fc82c101027b34425f3cfbdf6ff0f02f81f87
cindyweng18 <cindyweng18@gmail.com>
2024-07-24 17:57:48 -0400

    included icon on toggle

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -9,11 +9,8 @@ function Toggle() {
     let switched = false;
     const faceRef = useRef(null);
     const switchBGRef = useRef(null);
-
+    
     const switchToggle = () => {
-        // const face = document.querySelector(".innerSwitch");
-        // alert(faceElement[0].getBoundingClientRect().x)
-        // const switchBG = document.querySelector(".switch-background");
         const background = document.querySelector("body");
         const face = faceRef.current;
         const switchBG = switchBGRef.current;
@@ -53,7 +50,8 @@ function Toggle() {
             <div class="container">
                 <div class="switch-background off-shadow" ref={switchBGRef}>
                     <div class="innerSwitch" onClick={switchToggle}  ref={faceRef}>
-                        <svg width="100" height="100" viewBox="0 0 208 31" fill="none" xmlns="http://www.w3.org/2000/svg" class="face">
+                        <svg width="100" height="100" viewBox="-5 0 25 10" fill="none" xmlns="http://www.w3.org/2000/svg" class="face">
+                        <path class="light-dark" fill="black" d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"/>
                         </svg>
                     </div>
                 </div>
